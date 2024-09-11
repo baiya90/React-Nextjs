03c7f49b80537c64d1d1020af1909af370a817d7
baiya90 <zhanghaohbt@163.com>
2024-09-11 20:01:33 +0800

    feat: usePathname获取当前路由

---
+++ app/const/routePath.js
@@ -0,0 +1,6 @@
+export const  routePath = {
+    home: "/",
+    layoutDashboard: "/LayoutDashboard",
+    layoutDashboardSettings: '/LayoutDashboard/settings',
+    layoutDashboardAbout: "/LayoutDashboard/about",
+};
