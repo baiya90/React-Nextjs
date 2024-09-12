d30f92b42ebe23bad7729c88f68ca03ff20f66f1
baiya90 <zhanghaohbt@163.com>
2024-09-12 10:50:46 +0800

    feat: 认识路由组

---
+++ app/(routegroup)/route1/page.js
@@ -0,0 +1,12 @@
+
+
+export default function RouteGroup1() {
+ 
+  return (
+    <>
+      <div>路由组页面1</div>
+
+    </>
+    
+  )
+}
---
+++ app/(routegroup)/route2/page.js
@@ -0,0 +1,12 @@
+
+
+export default function RouteGroup2() {
+ 
+  return (
+    <>
+      <div>路由组页面2</div>
+
+    </>
+    
+  )
+}
